27e012d1a9508ce9e74419bcd5c5d91ced02b72a
sophie sorin <sophie4489@hotmail.fr>
2022-05-20 12:44:25 +0200

    adding update and delete list options

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
   resources :lists do
-      resources :bookmarks, only: [:new, :create, :destroy]
+      resources :bookmarks, only: [:new, :create, :destroy ]
     end
 end
