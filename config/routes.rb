c33a2a3c78e95ddb6ab3ba45082d3d74dc5dac54
sophie sorin <sophie4489@hotmail.fr>
2022-05-29 13:35:04 +0200

    updated the home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'lists#index'
+  root to: 'pages#home'
   resources :lists do
       resources :bookmarks, only: [:new, :create, :destroy ]
     end
